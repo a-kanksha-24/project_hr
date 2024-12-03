efe76439e12866d10e49806222a22dab56484a65
Pal2426 <akanksha.pal@triconinfotech.com>
2024-12-03 16:00:07 +0530

    bg image

--- client/src/components/cardDisplay.jsx
+++ client/src/components/cardDisplay.jsx
@@ -8,6 +8,10 @@ const Facilities = () => {
     <>
       <div className="background-section">
         <img src={background}></img>
+        <div className="header-text">
+        <h1>Swasthya Healthcare Centre</h1>
+        <p>Your health, our priority</p>
+      </div>
       </div>
       <div className="facilities-section">
         <h1 className="facilities-heading">Our Facilities</h1>
